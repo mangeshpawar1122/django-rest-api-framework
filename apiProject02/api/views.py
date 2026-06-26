9b29bb8889cd932d98cb2d239afb67795ec83110
mangeshpawar1122 <pawarmangesh112203@gmail.com>
2026-06-26 13:45:40 +0530

    create a delete method using api and all restapi form are a created and work a successfuly

--- apiProject02/api/views.py
+++ apiProject02/api/views.py
@@ -38,3 +38,13 @@ def update_student(request,pk):
     serializers.save()
     return Response(serializers.data,status=status.HTTP_200_OK)
   return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_student(request,pk):
+  try:
+    student = Student.objects.get(id=pk)
+  except Student.DoesNotExist:
+    return Response({"error":"student not fount"},status=status.HTTP_404_NOT_FOUND)
+  
+  student.delete()
+  return Response({"message":"student deleted successfully"},status=status.HTTP_204_NO_CONTENT)
