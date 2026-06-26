44b3c4380bf0288981855f02ba3c8dfcebd8e159
mangeshpawar1122 <pawarmangesh112203@gmail.com>
2026-06-26 13:42:43 +0530

    create a put and patch api and all work good

--- apiProject02/api/urls.py
+++ apiProject02/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('student/',views.get_student,name='get-student'),
     path('add/student/',views.create_student,name='create_student'),
+    path('student/update/<int:pk>/',views.update_student,name='update-student'),
+    path('student/delete/<int:pk>/',views.delete_student,name='delete_student'),
 ]
--- apiProject02/api/views.py
+++ apiProject02/api/views.py
@@ -20,3 +20,21 @@ def create_student(request):
     serializers.save()
     return Response(serializers.data,status=status.HTTP_201_CREATED)
   return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT','PATCH'])
+def update_student(request,pk):
+  try:
+    student = Student.objects.get(id=pk)
+  except Student.DoesNotExist:
+    return Response({"error":"student not fount"},status=status.HTTP_404_NOT_FOUND)
+  
+  # partial update support
+  if request.method == 'PATCH':
+    serializers = StudentSerializers(student,data=request.data,partial= True)
+  else:
+    serializers = StudentSerializers(student,data = request.data)
+  if serializers.is_valid():
+    serializers.save()
+    return Response(serializers.data,status=status.HTTP_200_OK)
+  return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
